8b3d0e30897ee64a31ab3fc04fbb9bbb2eb55cc4
Bob <dash78231@gmail.com>
2025-07-15 09:45:32 +0800

    change the name

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template:``,
+})
+export class AppComponent {
+  protected title = 'ecommerce-5';
+}
